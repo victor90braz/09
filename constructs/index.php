fc98b5494777529c6238cf15c0ba49fee153cf61
victor braz <victor.90braz@gmail.com>
2023-10-12 10:50:57 +0200

    exceptions

--- constructs/index.php
+++ constructs/index.php
@@ -3,3 +3,15 @@ $basePath = "C:\\Users\\braz9\\Desktop\\projects\\laracasts\\object-oriented-pri
 require_once $basePath . "functions.php";
 
 
+// exceptions
+
+function add($one, $two) {
+
+    if (! is_float($one) || ! is_float($two)) {
+        throw new \Exception('has to be an number ');
+    }
+
+    return $one + $two;
+}
+
+echo add(2,[]);
